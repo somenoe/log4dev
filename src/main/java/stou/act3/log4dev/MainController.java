cc26bd79eac4d0cb08c077a90be41019f0c80ad8
somenoe <otashi24@gmail.com>
2024-02-13 19:25:41 +0700

    🔥 remove unused func

--- src/main/java/stou/act3/log4dev/MainController.java
+++ src/main/java/stou/act3/log4dev/MainController.java
@@ -1,10 +1,6 @@
 package stou.act3.log4dev;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
@@ -14,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
@@ -26,34 +21,6 @@ public class MainController {
     List<Task> tasks = taskService.listAll(keyword);
     model.addAttribute("tasks", tasks);
     return "index";
-  }
-
-  @RequestMapping("/t")
-  public String showTimeDiff(Model model) {
-    String value = "";
-    String time1 = "15:16:04";
-    String time2 = "17:20:01";
-
-    SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-    try {
-      Date date1 = timeFormat.parse(time1);
-      Date date2 = timeFormat.parse(time2);
-
-      // milliseconds between the two dates
-      long difference = date2.getTime() - date1.getTime();
-
-      timeFormat = new SimpleDateFormat("HH:mm");
-      String formattedTime = timeFormat.format(new Date(difference));
-
-      value = formattedTime;
-    } catch (ParseException e) {
-      // handle the exception, e.g., log an error or throw a runtime exception
-      value = e.getMessage();
-    }
-    // model.addAttribute("value", value);
-    model.addAttribute("value", taskService.lastTask());
-    return "test";
-
   }
 
   @GetMapping("/edit/{id}")
